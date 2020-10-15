f85ffd31caf5eb59dcd27de4926c21e772d3fc65
Joseph Novakovich <josephnovakovich@gmail.com>
2020-10-15 13:04:36 -0400

    continued failure

--- jpl-module.c
+++ jpl-module.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <assert.h>
+#include <signal.h>
 #include <dlfcn.h>
 #include <pthread.h>
 typedef void V;typedef intmax_t I;typedef char C;typedef V* J;
@@ -17,6 +19,8 @@ int plugin_is_GPL_compatible;
 static JDT jdo;static JFT jfree;static JIT jinit;static JSXT jsmx;static JGT jgetr;
 static J gj; static C* gin; static C* gout;
 static C **adadbreak;
+static void sigint(int k){**adadbreak+=1;signal(SIGINT,sigint);}
+static void sigint2(int k){**adadbreak+=1;}
 static C* estring(EE* e, EV s)
 { DP sz=0; e->copy_string_contents(e,s,NULL,&sz);
   C *es=malloc(sz); e->copy_string_contents(e,s,es,&sz); R es; }
@@ -30,18 +34,24 @@ EFUN(jegetr)
 EFUN(jesmx)
 { J j=e->get_user_ptr(e,a[0]);C*o=estring(e,a[1]);freopen(o,"a",stdout);free(o);
   jsmx(j,jputs,NULL,NULL,NULL,2); R e->make_integer(e,0); }
-static V* jdoit (V*a) { jdo(gj,gin);gout=jgetr(gj); }
+static V* jdoit (V*a) { jdo(gj,gin);gout=jgetr(gj); R NULL; }
 EFUN(jegetrt)
 { gj=e->get_user_ptr(e,a[0]);C*s=estring(e,a[1]);gin = s;
-  jsmx(gj,jputs,NULL,NULL,NULL,2);
+  adadbreak=(char**)gj;
+  jsmx(gj,NULL,NULL,NULL,NULL,2);
   pthread_t t;
   pthread_create (&t,NULL,jdoit,NULL);
   int iter=0;
   printf("ok 2\n");
   while(pthread_tryjoin_np (t,NULL)) {
+    if (e->should_quit(e)) {
+      printf("request to quit!\n");
+      assert(0==pthread_detach(t));
+      break;
+    }
     iter++;
   }
-  printf("%d %s\n",iter,gout);
+  printf("iters: %d\nresult: %s\nlen result: %ld\n",iter,gout,strlen(gout));
   R e->make_string(e,gout,strlen(gout));
 }
 int emacs_module_init (ERT* rt)
