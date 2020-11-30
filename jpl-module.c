d0cce99d02808aed5a2c87c37e9408689611b37f
Joseph Novakovich <josephnovakovich@gmail.com>
2020-11-30 14:39:58 -0500

    minorly

--- jpl-module.c
+++ jpl-module.c
@@ -72,10 +72,6 @@ EFUN(jesetis) // int scalar
 { J j=e->get_user_ptr(e,a[0]);C*var=estring(e,a[1]);EV val=a[2];
   I jt=4,jr=0,*jl=NULL,*jd=malloc(sizeof(I));*jd=e->extract_integer(e,val);
   I r = jsetm(j,var,&jt,&jr,(I*)&jl,(I*)&jd);free(jd);R e->make_integer(e,r); }
-EFUN(jesetfs) // float scalar
-{ J j=e->get_user_ptr(e,a[0]);C*var=estring(e,a[1]);EV val=a[2];
-  I jt=8,jr=0,*jl=NULL;D*jd=malloc(sizeof(D));*jd=e->extract_float(e,val);
-  I r = jsetm(j,var,&jt,&jr,(I*)&jl,(I*)&jd);free(jd);R e->make_integer(e,r); }
 EFUN(jesetfv) // for now, assumes rank 1 float vector
 { J j=e->get_user_ptr(e,a[0]);C*var=estring(e,a[1]);EV val=a[2];
   I jt=8,jr=1,*jl=malloc(sizeof(I));*jl=e->vec_size(e,val);
@@ -83,6 +79,10 @@ EFUN(jesetfv) // for now, assumes rank 1 float vector
   DO(*jl,jd[i]=e->extract_float(e,e->vec_get(e,val,i)));
   I r = jsetm(j,var,&jt,&jr,(I*)&jl,(I*)&jd);
   free(jl);free(jd); R e->make_integer(e,r); }
+EFUN(jesetfs) // float scalar
+{ J j=e->get_user_ptr(e,a[0]);C*var=estring(e,a[1]);EV val=a[2];
+  I jt=8,jr=0,*jl=NULL;D*jd=malloc(sizeof(D));*jd=e->extract_float(e,val);
+  I r = jsetm(j,var,&jt,&jr,(I*)&jl,(I*)&jd);free(jd);R e->make_integer(e,r); }
 
 EFUN(jegetm) // nasty mess for now
 { J j=e->get_user_ptr(e,a[0]);C*v=estring(e,a[1]);I jt,jr,js,jd;
