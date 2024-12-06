ac53f13908a91811f9e9911bbf47254799b65a9c
jitwit <josephnovakovich@gmail.com>
2024-12-05 19:47:20 -0500

    discover escaping annoyance

--- test.el
+++ test.el
@@ -17,6 +17,8 @@
   (j-test-do/get "vb" "1 = i. 3" '[0 1 0])
   (j-test-do/get "vs" "a.{~65+i.26" "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
   (j-test-do/get "vi" "i. 2 2 2" '[[[0 1] [2 3]] [[4 5] [6 7]]])
+  ;; ISSUE ... is there some way to not have to escape \ and "?
+  (j-test-do/get "vi" "_2 ]\\i. 4" '[[0 1] [2 3]])
   (j-test-do/get "fv" "0.1 + i. 4" '[0.1 1.1 2.1 3.1])
   (j-test-do/get "pi" "1p1" (* 2 (acos 0)))
   ;; issue is chop, need to handle 0 properly
